df74c1374de98e4c18a09cc5581abb03d9c0e649
ilhyeonchu <ilhyeonchu@gmail.com>
2026-05-18 14:47:25 +0900

    3-1

---
+++ code/3-1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int (*BinOp)(int, int);
+
+int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
+int mul(int a, int b) { return a * b; }
+
+int compute(int a, int b, BinOp op) {
+  if (op == NULL) {
+    printf("Null pointer\n-1\n");
+    exit(0);
+  }
+  return op(a, b);
+}
+
+int main() {
+  int a = 10, b = 5;
+
+  printf("%d\n", compute(a, b, add));
+  printf("%d\n", compute(a, b, sub));
+  printf("%d\n", compute(a, b, mul));
+  printf("%d\n", compute(a, b, NULL));
+
+  return 0;
+}
